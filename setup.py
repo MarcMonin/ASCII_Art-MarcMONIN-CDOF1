0c48df8339cda31e80b3d92b10f98335bb13d390
marc <marc@lesmonin.com>
2025-01-13 16:03:22 +0100

    creation du setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+ name='{ASCII_Art-MarcMONIN-CDOF1}',
+ version='1.0',
+ author='{MarcMonin}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
